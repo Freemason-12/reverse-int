4d23d364eac497b3ab1269cbb59b88ef1e0d24a4
Maksat <maksatr@localhost.localdomain>
2023-07-29 12:04:34 +0600

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let d = Math.abs(n), res = 0
+    while (d > 0) { res *= 10; res += d % 10; d = Math.floor(d / 10); }
+    return res
 }
